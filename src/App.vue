ffe65ab4b6219c1f260713c4aae8c3be9b04989c
허규남 <heokyunam@naver.com>
2025-04-12 17:56:26 +0900

    Github Pages 배포 시도 - dist 폴더 git 허용

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import OracleSqlParser from './components/OracleSqlParser.vue';
 </script>
 
 <template>
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <oracle-sql-parser />
 </template>
 
 <style scoped>
---
+++ src/components/OracleSqlParser.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <textarea v-model="sql"/>
+    </div>
+</template>
+<script lang="ts" setup>
+    import { ref } from 'vue';
+
+    const sql = ref('');
+</script>
+<style scoped>
+    .container text-area {
+        width: 80vw;
+    }
+</style>
